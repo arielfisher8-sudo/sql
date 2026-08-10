d4886eae7b3e0beb352233c0d32c3b3b49508677
arielfisher8-sudo <arielfisher8@gmail.com>
2026-08-10 12:28:49 +0300

    Update SQLQuery5.sql

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -1,3 +1,4 @@
+--Doctors Appointment system
 create table appointments (DoctorName varchar(100) , PatientName varchar(100) , dateAndTime datetime, treatment varchar(100))
 
 with treatmentcount as (select treatment as treatment1, count(treatment) as treatmentcounter from appointments group by treatment)
